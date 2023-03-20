822ae237190891fb9d74a9e319154d35dfc7147c
Emmanuel Aje <53949905+Nailer@users.noreply.github.com>
2023-03-20 15:52:52 +0100

    still in progress

---
+++ post-page.js
@@ -0,0 +1,9 @@
+// element = document.querySelector(".products-voice")
+function myFunction() {
+    var element = document.getElementsByClassName("options-text");
+    element.classList.toggle("toggle");
+}
+
+// function toggles(){
+//     document.getElementById('function').style.opacity = '1.0';
+//  }
