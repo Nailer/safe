83a6fbfd34c1e8c07694981fb64e70357b9f2dce
Success Aje <73680210+successaje@users.noreply.github.com>
2023-03-20 05:57:14 +0100

    update product post

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 var movingText = document.getElementById("moving-text");
+// var audioButton = document.getElementsByClassName(".audio-description")
+var audBut = document.getElementById("myAudio")
 
 function toggleAnimation() {
   if (movingText.style.animationPlayState === "paused") {
@@ -8,3 +10,21 @@ function toggleAnimation() {
   }
 }
 
+
+function playera() {
+  audBut.play();
+}
+
+function pause() {
+  audBut.pause();
+}
+
+// audioButton.addEventListener("click", function play() {
+//   audBut.pause();
+// })
+
+// document.getElementById('play-description').addEventListener('click', function add () {
+//   var audio = new Audio('./Recording.m4a');
+//   audio.play() - 1
+//   add--
+// });
